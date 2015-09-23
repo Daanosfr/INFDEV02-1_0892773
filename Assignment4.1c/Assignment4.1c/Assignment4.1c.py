5581789801dbe7f40dddcd4f950516703bac799f
Daanosfr <0892773@hr.nl>
2015-09-23 10:20:21 +0200

    Assignment4.1c

---
+++ Assignment4.1c/Assignment4.1c/Assignment4.1c.py
@@ -0,0 +1,2 @@
+x = int(input("Give me an number? "))
+print ("the absolute value of this number is: ", abs(x))
--- starProject/starProject/starProject.py
+++ starProject/starProject/starProject.py
@@ -1,7 +1,8 @@
-x = 20
-print ("*", x * "*", "*")
-for n in range(1, x):
-    print( "*", (x - n) * "*", " " * (n - 1), "*")
-print ("*", x * "*", "*")
+﻿x = None
+while not x:
+    try:
+        x = int(input("How many rows? "))
+    except ValueError:
+        print ("Invalid Number")
     
 
