34254084573239a101dc39a5b17c844dc277e3b6
Daanosfr <0892773@hr.nl>
2015-10-05 14:35:28 +0200

    2.7ready

--- Assignment5.2/Assignment5.2/Assignment5.2.py
+++ Assignment5.2/Assignment5.2/Assignment5.2.py
@@ -1,5 +1,6 @@
-lengthCheck=True
-while lengthCheck:
+﻿import re
+running=True
+while running:
     p=raw_input("What is your Password? ")
     if len(p) <6:
         print "Your Password is too short, you must at least have 6 characters."
@@ -7,6 +8,17 @@ while lengthCheck:
         print "Your Password is too long, 12 characters is the maximum."
     if 6 <= len(p) <=12:
         print "Your password Length is OK."
-        lenghtCheck=False
+        running=False
+if re.search(r'[a-z]', p):
+    if re.search(r'[A-Z]' ,p):
+        if re.search(r'[d/]',p):
+            if re.search(r'[^a-zA-Z0-9_]', p):
+                print "Your password is super strong!"
+            else:
+                print "Your password is strong, try using special characters to strengthen your password."
+        else: "Your password is OK, try using some numbers and special characters to strengthen your password."
+    else: "Your password is too weak"
+else: "Your password is shit"
+            
 
-
+    
