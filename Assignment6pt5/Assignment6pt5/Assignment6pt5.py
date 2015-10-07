74f511f94c477b53af8b01f965452df73fdd647d
Daanosfr <0892773@hr.nl>
2015-10-07 08:52:45 +0200

    2.7 Ready

---
+++ Assignment6pt5/Assignment6pt5/Assignment6pt5.py
@@ -0,0 +1,16 @@
+﻿radius = input("Radius: ")
+a = radius #define a b and r to calculate the delta x and delta y in the following lines of code 
+b = radius
+r = radius
+width = 2 * radius + 1 #2 times the radius for the diameter. +1 for the range
+height = 2 * radius + 1
+
+
+map_ = [[' ' for x in range(width)] for y in range(height)] #creates the map-width and height and put's "space" on all points
+
+for y in range(height):
+    for x in range(width): 
+        if (x-a)**2 + (y-b)**2 - r**2 < r : 
+            map_[y][x] = '*' #draw a * on al spots for x & y
+for line in map_: #printing the map
+    print ' '.join(line) #creates a space between dots and asterisks. join for the sequence.
---
+++ assignment6pt6/assignment6pt6/assignment6pt6.py
@@ -0,0 +1,4 @@
+from Tkinter import Image
+
+image = Image.open('smiley.jpg')
+image.show()
